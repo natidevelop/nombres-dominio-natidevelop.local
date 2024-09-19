415008fd6d7307ca2bed88e10976e2264ba659f9
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-09-19 00:38:28 +0000

    dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload = function() {
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon", "telecom", "puedes"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let extensions = [`es`, `uy`, `ve`, `br`, `py`, `com`];
+
+let dominios = [];
+
+pronoun.forEach((start) => {
+  adj.forEach((mid) => {
+    noun.forEach((last) => {
+      extensions.forEach((ex) => {
+        let domain = `${start}${mid}${last}.${ex}`;
+
+        if (last.endsWith(ex)) {
+          let domainHack = `${start}${mid}${last.slice(
+            0,
+            -1 * ex.length
+          )}.${ex}`;
+          dominios.push(domainHack);
+        } else {
+          dominios.push(domain);
+        }
+      });
+    });
+  });
+});
+console.log(dominios);
