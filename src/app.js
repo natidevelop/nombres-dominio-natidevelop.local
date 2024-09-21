d62fcb76fef4d2d8d8a9ec3ad69d61d029099f39
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-09-21 00:53:35 +0000

    arreglos

--- src/app.js
+++ src/app.js
@@ -1,33 +1,38 @@
-// /* eslint-disable */
-// import "bootstrap";
-// import "./style.css";
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
-// window.onload = function() {
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon", "telecom", "puedes"];
+window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "telecom", "puedes"];
 
-let extensions = [`es`, `uy`, `ve`, `br`, `py`, `com`];
+  let extensions = [`es`, `uy`, `ve`, `br`, `py`, `com`];
 
-let dominios = [];
+  let dominios = [];
 
-pronoun.forEach((start) => {
-  adj.forEach((mid) => {
-    noun.forEach((last) => {
-      extensions.forEach((ex) => {
-        let domain = `${start}${mid}${last}.${ex}`;
+  pronoun.forEach(start => {
+    adj.forEach(mid => {
+      noun.forEach(last => {
+        extensions.forEach(ex => {
+          let domain = `${start}${mid}${last}.${ex}`;
 
-        if (last.endsWith(ex)) {
-          let domainHack = `${start}${mid}${last.slice(
-            0,
-            -1 * ex.length
-          )}.${ex}`;
-          dominios.push(domainHack);
-        } else {
-          dominios.push(domain);
-        }
+          if (last.endsWith(ex)) {
+            let domainHack = `${start}${mid}${last.slice(
+              0,
+              -1 * ex.length
+            )}.${ex}`;
+            dominios.push(domainHack);
+          } else {
+            dominios.push(domain);
+          }
+        });
       });
     });
   });
-});
-console.log(dominios);
+  let elementos = dominios.map(domainHack => {
+    return `<li class="alert alert-primary">` + domainHack + `</li>`;
+  });
+
+  document.getElementById("listadominios").innerHTML = elementos.join(" ");
+};
